ff20fd518e3576b6f3fa42213d030902d72c41e4
sion0104 <sio0104@naver.com>
2025-11-25 01:53:10 +0900

    refactor: replace existing ZStack implementation with reusable Checkbox component

--- RacingAI/RacingAI/View/Screens/SignUp/GenderOptionView.swift
+++ RacingAI/RacingAI/View/Screens/SignUp/GenderOptionView.swift
@@ -8,17 +8,7 @@ struct GenderOptionView: View {
     var body: some View {
         Button(action: action) {
             HStack {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 24, height: 24)
-                        .foregroundStyle(Color("Button"))
-                    
-                    if isSelected {
-                        Image(systemName: "checkmark")
-                            .foregroundStyle(.black)
-                    }
-                    
-                }
+                AppCheckBox(isOn: .constant(isSelected), size: 24)
                 Text(title)
                     .foregroundStyle(.black)
                     .fontWeight(.medium)
