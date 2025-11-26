0412c19962edf23dc1142369c480f717f98aae2f
sion0104 <sio0104@naver.com>
2025-11-26 17:55:43 +0900

    feat: implement onboarding start screen view

---
+++ RacingAI/RacingAI/View/Screens/Onboarding/FirstQuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstQuestionView.swift
+//  RacingAI
+//
+//  Created by 최시온 on 11/26/25.
+//
+
+import SwiftUI
+
+struct FirstQuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstQuestionView()
+}
---
+++ RacingAI/RacingAI/View/Screens/Onboarding/StartView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct StartView: View {
+    @State private var canNavigate: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("맞춤 플래너 사용을 위해\n몸 상태를 체크할게요")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.top, 40)
+                Text("부상 예방과 회복 속도 조절에 꼭 필요합니다")
+                    .font(.callout)
+                    .padding(.top, 5)
+                
+                Spacer()
+                
+                AppButton(
+                    title: "설문 시작하기",
+                    isEnabled: true
+                ) {
+                    canNavigate = true
+                }
+            }
+            .padding()
+            .navigationDestination(isPresented: $canNavigate) {
+                FirstQuestionView()
+            }
+        }
+
+    }
+}
+
+#Preview {
+    StartView()
+}
