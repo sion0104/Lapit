62270cc33d5f49bffa4a988751ab1ee3222c4732
sion0104 <sio0104@naver.com>
2025-12-01 23:41:02 +0900

    feat: add enum and associated property to BodyInforStore

--- RacingAI/RacingAI/Features/Onboarding/BodyInfoStore.swift
+++ RacingAI/RacingAI/Features/Onboarding/BodyInfoStore.swift
@@ -4,14 +4,21 @@ final class BodyInfoStore: ObservableObject {
     @Published var height: String = ""
     @Published var weight: String = ""
     @Published var bodyFatRate: String = ""
-    
     @Published var weightChange: WeightChange? = nil
+    @Published var trainingPartner: TrainingPartner? = nil
 }
 
 enum WeightChange: String, CaseIterable, Identifiable {
     case none = "변화 없음"
     case gain = "증량"
     case loss = "감량"
+    
+    var id: Self { self }
+}
+
+enum TrainingPartner: String, CaseIterable, Identifiable {
+    case solo = "혼자 훈련해요"
+    case withCoachOrTerm = "코치나 팀과 함께해요"
     
     var id: Self { self }
 }
---
+++ RacingAI/RacingAI/View/Components/Buttons/BottomBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomBar.swift
+//  RacingAI
+//
+//  Created by 최시온 on 12/1/25.
+//
+
+import SwiftUI
+
+struct BottomBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomBar()
+}
